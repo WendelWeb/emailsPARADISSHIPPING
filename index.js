67992feedae143703169a313b0be47b4476f8f15
WendelWeb <159454536+WendelWeb@users.noreply.github.com>
2025-04-03 08:06:43 -0700

    add my new website linka

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 // Comprehensive CORS Configuration
 const corsOptions = {
-  origin: ['http://localhost:5174', 'http://localhost:5173', 'https://pniceshipping.vercel.app', 'https://pniceshipping.com'], 
+  origin: ['http://localhost:5174', 'http://localhost:5173', 'https://pniceshipping.vercel.app', 'https://pniceshipping.com', 'https://www.pniceshipping.com'], 
   methods: ['GET', 'POST', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true, // Allow credentials (cookies, authorization headers)
